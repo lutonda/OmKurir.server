31e9b688011b9bfc912ba16d97af6c01f7e52be8
sebastiaolutonda <lutonda@gmail.com>
2022-06-06 22:01:44 +0000

    next

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,6 +7,7 @@ import User from "./user";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} = process.env
 const connection = new Sequelize({
   dialect: "mariadb",
